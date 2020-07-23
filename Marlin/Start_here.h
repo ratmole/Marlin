88e4a7ddb9f3808f4f8b459b824e038593b36fae
Vert <vert@vertshobbies.com>
2020-07-23 12:02:47 -0400

    clean up

--- Marlin/Start_here.h
+++ Marlin/Start_here.h
@@ -1,5 +1,5 @@
 #pragma once
-#define SHORT_BUILD_VERSION "Marlin 2 Bugfix Build 496"
+#define SHORT_BUILD_VERSION "Marlin 2 Bugfix Build 497"
 #define CUSTOM_MACHINE_NAME "3D Printer"
 
 // Ctrl+click to open links
